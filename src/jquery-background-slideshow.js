1dffdbaed766d8818ae7373a9e645b6d418978fc
Stefan Haack <shaack@gmail.com>
2019-12-28 13:28:14 +0100

    initial commit

---
+++ src/jquery-background-slideshow.js
@@ -0,0 +1,12 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/bootstrap-input-spinner
+ * License: MIT, see file 'LICENSE'
+ */
+
+;(function ($) {
+    "use strict"
+    $.fn.backgroundSlideshow = function (options) {
+
+    }
+}(jQuery))
